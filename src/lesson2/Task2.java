b0840237d4d00fd1e657e278174dc749586532bb
Yaroslav <yaroslav29-1994@mail.ru>
2020-09-16 09:43:46 +0300

    Поправил замечение

--- src/lesson2/Task2.java
+++ src/lesson2/Task2.java
@@ -8,9 +8,9 @@ public class Task2 {
         System.out.println("Введите число ");
         int number = in.nextInt();
         int result = 0;
-        String numbers = Integer.toString(number);
-        for (int i = 0; i < numbers.length(); i++) {
-            result += Integer.parseInt(String.valueOf(numbers.charAt(i)));
+        while (number != 0) {
+            result += (number % 10);
+            number /= 10;
         }
         System.out.println(result);
     }
